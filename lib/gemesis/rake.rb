ce43443e0f26b3ff82593839bcff760e2a913557
Mark Ryall <mark@ryall.name>
2010-03-08 17:15:44 +1100

    bumped version, renamed build task and added a desc

--- lib/gemesis/rake.rb
+++ lib/gemesis/rake.rb
@@ -6,22 +6,23 @@ namespace :gem do
   raise 'gemesis requires a git or mercurial repository' unless vcs
 
   desc "push new #{spec.name} release and #{vcs} tag"
-  task :push => :build_gem do
+  task :push => :build do
     sh "#{vcs} tag #{spec.version}" if vcs
     sh "gem push #{spec.name}-#{spec.version}.gem"
   end
 
-  task :build_gem do
+  desc "build #{spec.name} gem file"
+  task :build do
     sh "gem build gemspec"
   end
 
   desc "install #{spec.name} locally"
-  task :install => :build_gem do
+  task :install => :build do
     sh "gem install #{spec.name}-#{spec.version}.gem --no-ri --no-rdoc"
   end
 
   desc "uninstall #{spec.name} locally"
-  task :uninstall => :build_gem do
+  task :uninstall => :build do
     sh "gem uninstall #{spec.name} -x -a"
   end
 
